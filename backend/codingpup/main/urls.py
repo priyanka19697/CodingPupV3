b338425c63c7b8d0aabb55904d6b916d7f2bcaf2
Priyanka <priyanka19697@gmail.com>
2023-06-22 20:47:40 +0200

    add token auth

--- backend/codingpup/main/urls.py
+++ backend/codingpup/main/urls.py
@@ -18,10 +18,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
-from profiles.views  import ChangePasswordView
+from profiles.views  import ChangePasswordView, LogoutView, HomeView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from .openapi_info import openapi_info
+from rest_framework_simplejwt import views as jwt_views
 
 schema_view = get_schema_view(
     openapi_info,
@@ -29,6 +30,15 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('home/', HomeView.as_view(), name ='home'),
+
+    path('token/', 
+          jwt_views.TokenObtainPairView.as_view(), 
+          name ='token_obtain_pair'),
+    path('token/refresh/', 
+          jwt_views.TokenRefreshView.as_view(), 
+          name ='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('admin/', admin.site.urls),
--- backend/codingpup/profiles/urls.py
+++ backend/codingpup/profiles/urls.py
@@ -2,18 +2,15 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 from .viewsets import ProfileViewSet, UserViewSet
-from .views import ChangePasswordView
 
 app_name = "profiles"
 
+urlpatterns = [
+     
+]
 router = DefaultRouter()
 router.register(r'profiles', ProfileViewSet, basename='profiles')
 router.register(r'users', UserViewSet, basename='user')
-
-
-urlpatterns = [
-
-]
 
 urlpatterns +=  router.urls
 
